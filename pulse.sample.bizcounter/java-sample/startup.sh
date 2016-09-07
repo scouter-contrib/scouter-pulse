a36dc240f2ab237323d959634a30fc329f933985
Gunhee Lee <gunlee01@gmail.com>
2016-09-07 22:00:14 +0900

    done1

--- pulse.sample.bizcounter/java-sample/startup.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -classpath ./lib/commons-cli-1.2.jar:./lib/converter-gson-2.1.0.jar:./lib/gson-2.6.2.jar:./lib/okhttp-3.3.0.jar:./lib/okio-1.8.0.jar:./lib/pulse.lib.http-0.1.0-SNAPSHOT.jar:./lib/retrofit-2.1.0.jar:./pulse.sample.bizcounter-0.0.1-SNAPSHOT.jar scouterx.pulse.sample.bizcounter.App
